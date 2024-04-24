0ff2e677ce89cf768f855f9ece355d37711c3983
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2024-04-25 00:05:35 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (81.01%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,17 +1,15 @@
 class Solution {
-private:
-    int AkshatTheRobber(vector<int>& nums, int i,vector<int> &dp) {
-        if (i < 0)
-            return 0;
-        if(dp[i]!=-1)return dp[i];
-        int robCurrent = nums[i] + AkshatTheRobber(nums, i - 2,dp);
-        int skipCurrent = AkshatTheRobber(nums, i - 1,dp);
-        return dp[i]=max(robCurrent, skipCurrent);
-    }
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n,-1);
-        return AkshatTheRobber(nums, n - 1,dp);
+        if (nums.size() == 0)
+            return 0;
+        int prev1 = 0;
+        int prev2 = 0;
+        for (int num : nums) {
+            int tmp = prev1;
+            prev1 = max(prev2 + num, prev1);
+            prev2 = tmp;
+        }
+        return prev1;
     }
 };
