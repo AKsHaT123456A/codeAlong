c51dac432425f3c9cb8d246ca212ee4a06d542d5
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2024-04-27 23:47:46 +0530

    Time: 212 ms (100.00%) | Memory: 113.2 MB (25.00%) - LeetSync

---
+++ 3388-right-triangles/right-triangles.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    long long numberOfRightTriangles(vector<vector<int>>& grid) {
+        int n = grid[0].size(); // Width
+        int m = grid.size();    // Height
+
+        vector<int> rowSum(m, 0);
+        vector<int> columnSum(n, 0);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                rowSum[i] += grid[i][j];
+                columnSum[j] += grid[i][j];
+            }
+        }
+
+        long long ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans += (grid[i][j]) * (rowSum[i] - 1) * (columnSum[j] - 1);
+            }
+        }
+        return ans;
+    }
+};
