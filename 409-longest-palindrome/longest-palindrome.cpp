c0c7642906e0d6a139a2c33ad3855e493404bfc5
Akshat Mishra <akshat.srajan@gmail.com>
2024-06-05 03:41:51 +0530

    Time: 6 ms (16.63%) | Memory: 9 MB (26.05%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        map<char, int> palind;
-        int ans = 0;
-        int odd = 0;
-        for (auto i : s) {
-            palind[i]++;
-            ans += (palind[i] % 2 == 0) ? 2 : 0; 
-            if (palind[i] % 2 != 0) odd++; 
-            else odd--; 
+        int ans=0;
+        int n=s.length();
+        map<char, int> m;
+        for(int i=0;i<n;i++){
+            m[s[i]]++;
         }
-        return ans + (odd > 0 ? 1 : 0); 
+        bool isOdd=false;
+        map<char, int>::iterator itr;
+        for(itr=m.begin();itr!=m.end();itr++){
+            if(itr->second%2==1){
+                isOdd = true;
+            }
+            ans += (itr->second/2)*2;
+        }
+        return ans+(isOdd>0?1:0);
     }
 };
