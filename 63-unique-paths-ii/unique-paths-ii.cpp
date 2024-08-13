cf47040fbe76610b6a4a3d86c30b1df11cb377dc
Akshat Mishra <akshat.srajan@gmail.com>
2024-08-14 05:13:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (23.87%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -3,7 +3,7 @@ public:
     int compute(vector<vector<int>>& grid, vector<vector<int>>& dp, int m,
                 int n) {
         if (m == 0 && n == 0)
-           return dp[m][n] = grid[m][n] == 1 ? 0 : 1;
+           return dp[m][n]= 1;
         if (m < 0 || n < 0 || grid[m][n] == 1)return 0;  
         if (dp[m][n] != -1)
             return dp[m][n];
