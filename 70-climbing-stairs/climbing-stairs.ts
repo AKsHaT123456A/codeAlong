015995c4bbf354412a8fe779710952bbebabf4bc
Akshat Mishra <akshat.srajan@gmail.com>
2024-10-19 23:42:56 +0530

    Time: 0 ms (100.00%) | Memory: 50.4 MB (33.24%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.ts
+++ 70-climbing-stairs/climbing-stairs.ts
@@ -1,4 +1,4 @@
-function climb(n: number, dp:Array<number>): number {
+function climb(n: number, dp: Array<number>): number {
     if (n === 1 || n === 0) return 1;
     if (dp[n] !== -1) return dp[n];
     let jumpOne: number = climb(n - 1, dp);
@@ -9,5 +9,13 @@ function climb(n: number, dp:Array<number>): number {
 
 function climbStairs(n: number): number {
     let dp = new Array(n + 1).fill(-1);
-    return climb(n, dp);
+    dp[1] = 1;
+    dp[0] = 1;
+    for (let i = 1; i <= n; i++) {
+        let jumpOne: number = dp[i-1];
+        let jumpTwo: number = 0;
+        if (i >= 2) jumpTwo = dp[i-2];
+        dp[i] = jumpOne + jumpTwo;
+    }
+    return dp[n];
 }
