e73999bbeb521efcb426991675b80a4a6a187a50
Akshat Mishra <akshat.srajan@gmail.com>
2024-11-16 19:23:21 +0530

    Time: 3 ms (46.25%) | Memory: 33.2 MB (55.99%) - LeetSync

---
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+   vector<int> resultsArray(vector<int>& nums, int k) {
+        int left=0, right=0;
+        int n = nums.size();
+        if(k==1) return nums;
+        bool flag = true;
+        vector<int> ans;
+        while(right<n){
+            if(right-left+1==k){
+                flag = true;
+                int maxEle=-1;
+                for(int i=left;i<right;i++){
+                    if((nums[i+1]-nums[i])==1)maxEle=max(maxEle,nums[i+1]);
+                    else flag = false;
+                }
+                if(!flag) maxEle = -1;
+                ans.push_back(maxEle);
+                left++;
+            }
+            right++;
+        }
+        return ans;
+    }
+};
