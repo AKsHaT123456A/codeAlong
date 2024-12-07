be977f5e6efc0c90090b46d84724d5f449a5010c
Akshat Mishra <akshat.srajan@gmail.com>
2024-12-07 13:15:30 +0530

    Time: 2 ms (52.06%) | Memory: 20.5 MB (15.96%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,23 +1,35 @@
 class Solution {
-public:
-    int findCircleNum(vector<vector<int>>& M) {
-        if (M.empty()) return 0;
-        int n = M.size();
-        vector<bool> visited(n, false);
-        int groups = 0;
-        for (int i = 0; i < visited.size(); i++) {
-            groups += !visited[i] ? dfs(i, M, visited), 1 : 0;
-        }
-        return groups;
-    }
-
 private:
-    void dfs(int i, vector<vector<int>>& M, vector<bool>& visited) {
-        visited[i] = true;
-        for (int j = 0; j < visited.size(); j++) {
-            if (i != j && M[i][j] && !visited[j]) {
-                dfs(j, M, visited);
+    void dfs(int node, vector<int>& vis, vector<vector<int>>& adj) {
+        vis[node] = 1;
+        for (auto neighbor : adj[node]) {
+            if (!vis[neighbor]) {
+                dfs(neighbor, vis, adj);
             }
         }
+    }
+
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<vector<int>> adj(n); // Adjacency list representation
+        vector<int> vis(n, 0);      // Visited array
+        // Construct adjacency list from isConnected matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1 && i != j) {
+                    adj[i].push_back(j);
+                }
+            }
+        }
+
+        int province = 0;
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) { // If the node is not visited
+                province++;
+                dfs(i, vis, adj); // Perform DFS
+            }
+        }
+        return province;
     }
 };
