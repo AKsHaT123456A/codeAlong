cd67537aa26249ba0f3a631b2cab943e75f5adbb
Akshat Mishra <akshat.srajan@gmail.com>
2024-12-09 01:22:19 +0530

    Time: 7 ms (82.24%) | Memory: 30.7 MB (70.71%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,47 +1,44 @@
 class Solution {
 public:
     int numEnclaves(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-        auto seeOne = [&](int row, int col) {
-            queue<pair<int, int>> q;
-            q.push({row, col});
+        int n = grid.size(), m = grid[0].size();
+        vector<pair<int, int>> directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        auto dfs = [&](int row, int col) {
+            stack<pair<int, int>> stk;
+            stk.push({row, col});
             grid[row][col] = 0;
-            while (!q.empty()) {
-                auto [r, c] = q.front();
-                q.pop();
-                for (auto& d : dir) {
-                    int nr = r + d[0];
-                    int nc = c + d[1];
+
+            while (!stk.empty()) {
+                auto [r, c] = stk.top();
+                stk.pop();
+                for (auto& [dr, dc] : directions) {
+                    int nr = r + dr, nc = c + dc;
                     if (nr >= 0 && nr < n && nc >= 0 && nc < m &&
                         grid[nr][nc] == 1) {
-                        q.push({nr, nc});
+                        stk.push({nr, nc});
                         grid[nr][nc] = 0;
                     }
                 }
             }
         };
-         for (int i = 0; i < n; i++) {
+
+        for (int i = 0; i < n; i++) {
             if (grid[i][0] == 1)
-                seeOne(i, 0);
+                dfs(i, 0);
             if (grid[i][m - 1] == 1)
-                seeOne(i, m - 1);
+                dfs(i, m - 1);
         }
         for (int j = 0; j < m; j++) {
             if (grid[0][j] == 1)
-                seeOne(0, j);
+                dfs(0, j);
             if (grid[n - 1][j] == 1)
-                seeOne(n - 1, j);
+                dfs(n - 1, j);
         }
-        int countOnes = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
-                    countOnes++;
-                }
-            }
+        int enclaveCount = 0;
+        for (const auto& row : grid) {
+            enclaveCount += count(row.begin(), row.end(), 1);
         }
-        return countOnes;
+
+        return enclaveCount;
     }
 };
