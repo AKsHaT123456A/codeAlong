5aa3e6b06e4560b80b85f9482b8a13a118577d1e
Akshat Mishra <akshat.srajan@gmail.com>
2025-01-05 08:10:12 +0530

    Time: 7 ms (91.84%) | Memory: 60.4 MB (73.47%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.ts
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.ts
@@ -1,25 +1,23 @@
 function numberOfSubarrays(nums: number[], k: number): number {
-    let right = 0;
     let left = 0;
+    let right = 0;
+    let prefix = 0;
     let oddCount = 0;
     let ans = 0;
-    let prefix = 0;  
-
     while (right < nums.length) {
-        if (nums[right] % 2 !== 0) {
+        if (nums[right] % 2 != 0) {
             oddCount++;
             prefix = 0;
         }
-
         while (oddCount === k) {
-            if (nums[left] % 2 !== 0) oddCount--;
+            if (nums[left] % 2 != 0) {
+                oddCount--;
+            }
             left++;
             prefix++;
         }
-
         ans += prefix;
         right++;
     }
-
     return ans;
-}
+};
